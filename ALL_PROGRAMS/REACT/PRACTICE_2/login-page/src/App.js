24db5cd487bdef76eae45f4ccf8ab772ad1f88b7
Arjun Gade <139616230+gadearjun24@users.noreply.github.com>
2024-07-31 19:02:46 +0000

    login-page

--- ALL_PROGRAMS/REACT/PRACTICE_2/login-page/src/App.js
+++ ALL_PROGRAMS/REACT/PRACTICE_2/login-page/src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <>
-      {/* <SignUp></SignUp> */}
       <center>
         <h1>WELCOME</h1>
       </center>
