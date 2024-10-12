4680d4bf2e9eb099e56176df883907643cde9a49
Arjun Gade <139616230+gadearjun24@users.noreply.github.com>
2024-10-12 13:14:34 +0000

    react-rouder-dom app

--- ALL_PROGRAMS/REACT/PRACTICE/chapter_1/src/components/Suggetion.js
+++ ALL_PROGRAMS/REACT/PRACTICE/chapter_1/src/components/Suggetion.js
@@ -1,15 +1,27 @@
 import "./Search.css";
 
-function Suggetion({ suggetion }) {
+function Suggetion({ suggetion, setSearchValue, newFunction }) {
+  function handleOnClickSpan(e) {
+    console.log(e.target.innerText);
+    // searchValue(e.target.innerText);
+    setSearchValue(e.target.innerText);
+  }
+
   return (
     <>
-      {/* {console.log("sugetionnnnnnnnn", suggetion)} */}
-
-      <div className="suggetion">
-        {suggetion.map((ele) => {
-          return <span>{ele}</span>;
-        })}
-      </div>
+      {suggetion === "" ? (
+        ""
+      ) : (
+        <div className="suggetion">
+          {suggetion.map((ele, index) => {
+            if (index < 5) {
+              return <span onClick={handleOnClickSpan}>{ele}</span>;
+            } else {
+              return "";
+            }
+          })}
+        </div>
+      )}
     </>
   );
 }
